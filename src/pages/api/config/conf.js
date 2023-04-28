fd7a9caed6de86df1dbbd78dfce4d7526b1de979
jamshidbekml <jamshidbekml@gmail.com>
2023-04-28 20:46:28 +0500

    delete incorrect files

--- src/pages/api/config/conf.js
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-
-config();
-
-const { env } = process;
-export const MONGO_URI = env.MONGO_URL || 'mongodb://127.0.0.1:27017/uzstore';
--- src/pages/api/modules/shared/utils/errorResponse.js
+++
@@ -1,7 +0,0 @@
-export default class ErrorResponse extends Error {
-    constructor(status, message) {
-        super(message);
-        this.status = status;
-        this.message = message;
-    }
-}
