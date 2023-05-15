13816c34213501f394e890c522d384dba461d6cd
jamshidbekml <jamshidbekml@gmail.com>
2023-05-15 16:57:38 +0500

    fix

---
+++ src/models/reports.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const reportSchema = new mongoose.Schema(
+    {
+        file_name: {
+            type: String,
+            required: true,
+        },
+        html: {
+            type: String,
+            required: true,
+        },
+        user: {
+            type: String,
+            ref: 'users',
+        },
+        company: {
+            type: String,
+            ref: 'companies',
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Reports =
+    mongoose.models.reports || mongoose.model('reports', reportSchema);
+export default Reports;
