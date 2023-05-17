73013ed01a77cf078dd504d0d58e612a0b04c3d1
jamshidbekml <jamshidbekml@gmail.com>
2023-05-17 22:55:10 +0500

    fix

---
+++ src/pages/api/admin/user/update-admin.js
@@ -0,0 +1,31 @@
+import nc from 'next-connect';
+import User from '../../../../models/user';
+import db from '../../../../utils/db';
+import { checkUserRole } from '../../../../utils/auth';
+
+const handler = nc();
+
+handler.use(checkUserRole('superadmin'));
+handler.put(async (req, res) => {
+    try {
+        const { users } = req.body;
+        await db.connect();
+
+        users.forEach(async (id) => {
+            let user = await User.findById(id);
+            if (user) {
+                await User.findByIdAndUpdate(id, { isBlock });
+            }
+        });
+
+        await db.disconnect();
+
+        res.status(200).send({
+            message: 'Users updated successfully',
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+export default handler;
