c07ce3a1d378bbcd4ac7d9b5d211cd019e70c612
jamshidbekml <jamshidbekml@gmail.com>
2023-05-17 20:22:01 +0500

    fix

--- src/pages/api/user/filtered-reports.js
+++ src/pages/api/user/filtered-reports.js
@@ -2,24 +2,22 @@ import nc from 'next-connect';
 import Reports from '../../../models/reports';
 import db from '../../../utils/db';
 import { isAuth } from '../../../utils/auth';
+import { htmlFilter } from '../../../utils/htmlReader';
 
 const handler = nc();
 
 handler.use(isAuth);
 handler.get(async (req, res) => {
     try {
+        const { id, category } = req.params;
         await db.connect();
-        const reports = await Reports.find({
-            company: req.user.company,
-            user: { $ne: req.user._id },
-        })
-            .select('-html')
-            .populate('user', 'email');
-
+        const report = await Reports.findById(id).select('html');
         await db.disconnect();
-        res.status(200).send({
-            reports,
-        });
+
+        const data = htmlFilter(report.html, category);
+
+        res.setHeader('Content-Type', 'text/html');
+        res.status(200).send(data);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
