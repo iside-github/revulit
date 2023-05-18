62be8af590f75873c55e81f2dfc68879eedb4d61
jamshidbekml <jamshidbekml@gmail.com>
2023-05-19 03:55:47 +0500

    fix

--- src/pages/api/admin/user/all-users.js
+++ src/pages/api/admin/user/all-users.js
@@ -1,5 +1,4 @@
 import nc from 'next-connect';
-import bcrypt from 'bcryptjs';
 import User from '../../../../models/user';
 import db from '../../../../utils/db';
 import { checkUserRole } from '../../../../utils/auth';
@@ -10,16 +9,25 @@ handler.use(checkUserRole('admin'));
 handler.get(async (req, res) => {
     try {
         await db.connect();
-        const admin = await User.findById(req.user.user.user._id);
+        const admin = await User.findById(req.user.user.user._id).populate({
+            path: 'company',
+            select: 'name',
+        });
+        console.log(admin);
         const users = await User.aggregate([
             {
                 $match: {
-                    company: admin.company,
+                    company: admin.company._id.toString(),
                     roles: {
                         $not: {
                             $elemMatch: { $regex: 'admin', $options: 'i' },
                         },
                     },
+                },
+            },
+            {
+                $addFields: {
+                    company: admin.company.name,
                 },
             },
             {
@@ -38,6 +46,7 @@ handler.get(async (req, res) => {
                     avatar: 1,
                     createdAt: 1,
                     reportCount: { $size: '$reportsCount' },
+                    company: 1,
                 },
             },
         ]);
