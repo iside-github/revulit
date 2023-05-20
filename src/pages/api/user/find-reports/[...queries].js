cc20a7416aaa4e6de656ecb4c0994002302ebfad
jamshidbekml <jamshidbekml@gmail.com>
2023-05-20 18:20:22 +0500

    fix

--- src/pages/api/user/find-reports/[...queries].js
+++ src/pages/api/user/find-reports/[...queries].js
@@ -2,29 +2,49 @@ import nc from 'next-connect';
 import Reports from '../../../../models/reports';
 import db from '../../../../utils/db';
 import { isAuth } from '../../../../utils/auth';
-import { htmlFilter } from '../../../../utils/htmlReader';
 
 const handler = nc();
 
 handler.use(isAuth);
 handler.get(async (req, res) => {
     try {
-        const [id] = req.query.queries;
-        const category = req.query.category;
+        const userName = req.query.name;
         await db.connect();
-        const report = await Reports.findById(id).select('html');
+        const reports = await Reports.aggregate([
+            {
+                $lookup: {
+                    from: 'users',
+                    localField: 'user',
+                    foreignField: '_id',
+                    as: 'user',
+                },
+            },
+            {
+                $addFields: {
+                    userName: { $first: '$user.name' },
+                },
+            },
+            {
+                $match: {
+                    userName: {
+                        $regex: userName,
+                        $options: 'i',
+                    },
+                },
+            },
+            {
+                $project: {
+                    html: 0,
+                    user: 0,
+                },
+            },
+            {
+                $limit: 10,
+            },
+        ]);
         await db.disconnect();
 
-        var data =
-            category !== 'total'
-                ? await new Promise((resolve) => {
-                      let html = htmlFilter(report.html, category);
-                      resolve(html);
-                  })
-                : report.html;
-
-        res.setHeader('Content-Type', 'text/html');
-        res.status(200).send(data);
+        res.status(200).send({ reports });
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
