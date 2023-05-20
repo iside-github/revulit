bbffaefbd4c8e2c7ef5380fa9378b860a29abe31
shohruzjon_vl <shohruzjon@vanalauri.com>
2023-05-20 18:52:55 +0500

    update

--- src/components/superadmin/CompanyList.js
+++ src/components/superadmin/CompanyList.js
@@ -16,7 +16,9 @@ import { Stack } from "@mui/system";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import axios from "axios";
-import { toast } from "react-hot-toast";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { getCompaniesList } from "redux-store/company/index.slice";
 
 export const CompanyListTable = (props) => {
   const {
@@ -42,13 +44,28 @@ export const CompanyListTable = (props) => {
   );
 
   const enableBulkActions = selectedCustomers.length > 0;
+  const dispatch = useDispatch();
 
-  const handleBlockUnblock = (company, status) => {
-    toast.promise(async () => {}, {
-      loading: "Loading...",
-      success: "Company blocked",
-      error: "Unexpected error",
-    });
+  const handleBlockUnblock = async (status, id) => {
+    try {
+      await axios({
+        method: "POST",
+        url: `/api/admin/company/update-company/${id}`,
+        data: {
+          isBlock: !status,
+          id,
+        },
+      });
+      dispatch(getCompaniesList());
+      toast.success("Action completed");
+    } catch (error) {
+      console.log(error);
+      toast.error(
+        error?.response?.data?.message
+          ? error?.response?.data?.message
+          : "Unexpected error. Try again later"
+      );
+    }
   };
 
   return (
@@ -106,7 +123,9 @@ export const CompanyListTable = (props) => {
                     </TableCell>
                     <TableCell align="right">
                       <IconButton
-                        onClick={handleBlockUnblock}
+                        onClick={() =>
+                          handleBlockUnblock(customer?.isBlock, customer?._id)
+                        }
                         sx={{ borderRadius: "50%" }}
                       >
                         {customer?.isBlock ? (
